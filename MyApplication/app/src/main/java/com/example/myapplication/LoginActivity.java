501d639daed3ccecc46f7367155df9e395b6605a
Jiasheng Li <u7758372@anu.edu.au>
2023-10-11 06:27:43 +1100

    Add Login Activity designed by Zhining Zhang

---
+++ MyApplication/app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -0,0 +1,36 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        EditText email_imput = findViewById(R.id.email_input);
+        EditText password_imput = findViewById(R.id.password_input);
+
+        Button loginButton = findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = email_imput.getText().toString();
+                String password = password_imput.getText().toString();
+
+                if ((email.equals("comp2100@anu.edu.au") && password.equals("comp2100")) || (email.equals("comp6442@anu.edu.au") && password.equals("comp6442"))) {
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+}
