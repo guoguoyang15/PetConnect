68bea67d678bfd53db52e5134180629d17a3e1b5
Jiasheng Li <u7758372@anu.edu.au>
2023-10-16 20:52:33 +1100

    Change layout of Detail Activity.

--- MyApplication/app/src/main/java/com/example/myapplication/DetailActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/DetailActivity.java
@@ -1,14 +1,16 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -32,7 +34,16 @@ public class DetailActivity extends AppCompatActivity {
         TextView color = findViewById(R.id.petColor);
         color.setText("Pet Color: " + pet.color);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,pet.comment);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,pet.comment){
+            @NonNull
+            @Override
+            public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                TextView text = (TextView) view.findViewById(android.R.id.text1);
+                text.setTextColor(Color.WHITE);
+                return view;
+            }
+        };
         ListView comment = findViewById(R.id.commentList);
         comment.setAdapter(adapter);
     }
